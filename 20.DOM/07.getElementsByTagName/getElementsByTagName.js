c0bb0d856248722d70bf00595c5e715bcaf91451
Shivansh <shivanshkumar760@gmail.com>
2023-06-13 11:39:31 +0530

    Eigth Commit

--- 20.DOM/07.getElementsByTagName/getElementsByTagName.js
+++ 20.DOM/07.getElementsByTagName/getElementsByTagName.js
@@ -1,4 +1,4 @@
 console.log(document.getElementsByTagName("li"))
 let items =document.getElementsByTagName("li")//lsit return or array
-let element=items[0];
-let change=element.style.color="Blue"
+let element=items[1];
+let change=element.style.color="red";
